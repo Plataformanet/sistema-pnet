65e15262a894c277201ad0301812bd33576b58a3
alanvf1 <alan.varela404@gmail.com>
2026-06-03 11:36:37 -0300

    separei categorias e subcategorias no menu de navegação

--- resources/js/layouts/tenant-layout/TenantLayout.vue
+++ resources/js/layouts/tenant-layout/TenantLayout.vue
@@ -147,7 +147,7 @@ const data: TenantNav = {
         {
             title: "Documentações",
             module: "documents",
-            url: "/documentation/dashboard",
+            url: "#",
             items: [
                 {
                     title: "Propostas",
@@ -169,12 +169,17 @@ const data: TenantNav = {
         {
             title: "Financeiro",
             module: "finance",
-            url: "/finance/dashboard",
+            url: "#",
             items: [
                 {
-                    title: "Categorias/Subcategorias",
+                    title: "Categorias",
                     permission: "finance.categories.view",
                     url: "/finance/categories/list",
+                },
+                {
+                    title: "Subcategorias",
+                    permission: "finance.subcategories.view",
+                    url: "/finance/subcategories/list",
                 },
                 {
                     title: "Contas Bancárias",
